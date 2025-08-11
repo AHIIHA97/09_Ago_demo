a945303a5d6a553f6bda66590e2a1452d044e946
AHIIHA97 <federica.zenoni.1@gmail.com>
2025-08-11 13:52:31 +0200

    Update hello1.py
    commit5 to simulate merge conflict

--- hello1.py
+++ hello1.py
@@ -1,11 +1,11 @@
 # funct to return index & letter mapping
 def cnt (i_minim:int=1, i_max:int=5, pace:int=1)->dict:
-    store = {}
+    storep = {}
     for i in range(i_minim,i_max,pace):
         print (f"item {i}, letter:{chr(96+i)}")
-        store[i] = chr(96 +i)
+        storep[i] = chr(96 +i)
     #return dictionary with the current item and corresponding letter
-    return store
+    return storep
 
-dict = cnt(1, 6, 1)
-print(dict.items())
+dictio = cnt(1, 6, 1)
+print(dictio.items())

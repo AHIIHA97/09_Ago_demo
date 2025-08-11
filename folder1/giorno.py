bc44b72e37ef4db963068b1a0620000d9c162838
AHIIHA97 <federica.zenoni.1@gmail.com>
2025-08-11 14:37:29 +0200

    Update giorno.py
    commt 10

--- folder1/giorno.py
+++ folder1/giorno.py
@@ -1,9 +1,10 @@
 ##heyhey
 print("du bist dumm!")
-i = 5
-y = 6
-def sumf (n1, n2):
+i = 55
+y = 66
+def sumfe (n1, n2):
   su = n1+n2
   return su
 
-susu = sumf(23.484 , 33.978)
+susu = sumfe(23.484 , 33.978)
+
